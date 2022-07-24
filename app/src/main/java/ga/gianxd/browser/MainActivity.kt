24b6f7af5a20aee1f1689d2de98cfc49f0c3057a
Gian Borcillo <borcillofg2020@gmail.com>
2022-07-24 21:25:57 +0800

    feat: Implemented some WebView test

--- app/src/main/java/ga/gianxd/browser/MainActivity.kt
+++ app/src/main/java/ga/gianxd/browser/MainActivity.kt
@@ -2,10 +2,23 @@ package ga.gianxd.browser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ga.gianxd.browser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        createRootView()
+        loadTempUrl()
+    }
+
+    private fun loadTempUrl() {
+        binding.testWebView.loadUrl("https://gianxd.ga")
+    }
+
+    private fun createRootView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
