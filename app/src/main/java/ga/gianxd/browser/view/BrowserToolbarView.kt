a6e926598405414be886e7cc71d368f806476ab5
Gian Borcillo <borcillofg2020@gmail.com>
2022-07-28 19:35:30 +0800

    style: Code changes; refactor: Remove unnecessary imports

--- app/src/main/java/ga/gianxd/browser/view/BrowserToolbarView.kt
+++ app/src/main/java/ga/gianxd/browser/view/BrowserToolbarView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.core.view.isVisible
