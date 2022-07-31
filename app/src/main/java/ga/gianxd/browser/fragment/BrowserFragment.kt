e3cd260f6dc070e83df70ee13de60ed57236d675
Gian Borcillo <borcillofg2020@gmail.com>
2022-07-31 18:55:58 +0800

    fix: BrowserFragment crashing when going to preferences

--- app/src/main/java/ga/gianxd/browser/fragment/BrowserFragment.kt
+++ app/src/main/java/ga/gianxd/browser/fragment/BrowserFragment.kt
@@ -23,11 +23,61 @@ class BrowserFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var actionBar: ActionBar
-
     private lateinit var webSession: GeckoSession
 
     private var canGoBack = false
     private var canGoForward = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webUserAgent = "Mozilla/5.0 (Linux; Android ${android.os.Build.VERSION.RELEASE}; ${android.os.Build.MODEL}) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) ArchoBrowser/100.0.20220425210429 Mobile Safari/537.36"
+        val webSessionSettings = GeckoSessionSettings.Builder()
+            .allowJavascript(true)
+            .userAgentOverride(webUserAgent)
+            .build()
+
+        webRuntime = GeckoRuntime.create(requireContext())
+        webSession = GeckoSession(webSessionSettings)
+
+        webSession.contentDelegate = object: GeckoSession.ContentDelegate {}
+        webSession.progressDelegate = object: GeckoSession.ProgressDelegate {
+            override fun onPageStart(session: GeckoSession, url: String) {
+                super.onPageStart(session, url)
+                binding.toolbar.progress.isVisible = true
+
+                if (binding.toolbar.url.text.toString() != url)
+                    binding.toolbar.url.setText(url)
+            }
+
+            override fun onPageStop(session: GeckoSession, success: Boolean) {
+                super.onPageStop(session, success)
+                binding.toolbar.progress.isVisible = false
+            }
+
+            override fun onProgressChange(session: GeckoSession, progress: Int) {
+                super.onProgressChange(session, progress)
+                binding.toolbar.progress.progress = progress
+            }
+        }
+        webSession.navigationDelegate = object: GeckoSession.NavigationDelegate {
+            override fun onLocationChange(session: GeckoSession, url: String?) {
+                super.onLocationChange(session, url)
+                binding.toolbar.url.setText(url)
+            }
+
+            override fun onCanGoBack(session: GeckoSession, canGoBack: Boolean) {
+                super.onCanGoBack(session, canGoBack)
+                this@BrowserFragment.canGoBack = canGoBack
+            }
+
+            override fun onCanGoForward(session: GeckoSession, canGoForward: Boolean) {
+                super.onCanGoForward(session, canGoForward)
+                this@BrowserFragment.canGoForward = canGoForward
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -78,63 +128,20 @@ class BrowserFragment : Fragment() {
             (requireActivity() as MainActivity).switch(MainActivity.FRAGMENT_PREFERENCES)
         }
 
-        val webUserAgent = "Mozilla/5.0 (Linux; Android ${android.os.Build.VERSION.RELEASE}; ${android.os.Build.MODEL}) " +
-                "AppleWebKit/537.36 (KHTML, like Gecko) ArchoBrowser/100.0.20220425210429 Mobile Safari/537.36"
-        val webSessionSettings = GeckoSessionSettings.Builder()
-            .allowJavascript(true)
-            .userAgentOverride(webUserAgent)
-            .build()
-
-        webSession = GeckoSession(webSessionSettings)
-
-        webSession.contentDelegate = object: GeckoSession.ContentDelegate {}
-        webSession.progressDelegate = object: GeckoSession.ProgressDelegate {
-            override fun onPageStart(session: GeckoSession, url: String) {
-                super.onPageStart(session, url)
-                binding.toolbar.progress.isVisible = true
-
-                if (binding.toolbar.url.text.toString() != url)
-                    binding.toolbar.url.setText(url)
-            }
-
-            override fun onPageStop(session: GeckoSession, success: Boolean) {
-                super.onPageStop(session, success)
-                binding.toolbar.progress.isVisible = false
-            }
-
-            override fun onProgressChange(session: GeckoSession, progress: Int) {
-                super.onProgressChange(session, progress)
-                binding.toolbar.progress.progress = progress
-            }
-        }
-        webSession.navigationDelegate = object: GeckoSession.NavigationDelegate {
-            override fun onLocationChange(session: GeckoSession, url: String?) {
-                super.onLocationChange(session, url)
-                binding.toolbar.url.setText(url)
-            }
-
-            override fun onCanGoBack(session: GeckoSession, canGoBack: Boolean) {
-                super.onCanGoBack(session, canGoBack)
-                this@BrowserFragment.canGoBack = canGoBack
-            }
-
-            override fun onCanGoForward(session: GeckoSession, canGoForward: Boolean) {
-                super.onCanGoForward(session, canGoForward)
-                this@BrowserFragment.canGoForward = canGoForward
-            }
-        }
-
-        webRuntime = GeckoRuntime.create(requireContext())
-        webSession.open(webRuntime!!)
-        webSession.loadUri("https://github.com/gianxddddd/archo-browser")
+        if (webRuntime != null && !webSession.isOpen) webSession.open(webRuntime!!)
         binding.webContents.setSession(webSession)
+        webSession.loadUri("https://github.com/gianxddddd/archo-browser")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding.webContents.releaseSession()
-        webRuntime?.shutdown()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webRuntime?.shutdown()
     }
 
     override fun onStart() {
